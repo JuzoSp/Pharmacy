11ea0451fdf2a31bad2938db01b9bab62dd2b1b7
JuzoSp <speyerhasina@gmail.com>
2024-01-31 16:56:39 +0300

    okok

--- Pharmacie/resources/views/auth/login.blade.php
+++ Pharmacie/resources/views/auth/login.blade.php
@@ -1,69 +1,47 @@
-<!DOCTYPE html>
-<html lang="fr">
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>My App</title>
-    <link rel="stylesheet" href="{{ asset('fonts/bootstrapIcons/bootstrap-icons.css') }}">
-    @vite('resources/css/app.css')
-</head>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-<body>
-    <div class="flex flex-col w-full justify-center items-center text-gray-600">
-        <div class="flex flex-row w-full h-[54px] justify-between items-center p-3 bg-blue-300">
-            <span>My App</span>
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex flex-col mx-auto bg-white border-[1px] border-gray-100 shadow-sm rounded-md mt-2 my-5">
-            <div class="flex justify-center w-full h-full my-auto xl:gap-14 lg:justify-normal md:gap-5 draggable">
-                <div class="flex items-center justify-center w-full lg:p-12">
-                    <form class="flex flex-col w-full h-full pb-6 text-center bg-white rounded-3xl">
-                        <h3 class="mb-3 text-4xl font-extrabold text-dark-grey-900">Se connecter</h3>
-                        <p class="mb-4 text-grey-700">Entrez votre e-mail et mot de passe</p>
-                        <a
-                            class="flex items-center justify-center w-full py-4 mb-6 text-sm font-medium transition duration-300 rounded-2xl text-grey-900 bg-grey-300 hover:bg-grey-400 focus:ring-4 focus:ring-grey-300">
-                            <img class="h-5 mr-2"
-                                src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/logos/logo-google.png"
-                                alt="">
-                            Sign in with Google
-                        </a>
-                        <div class="flex items-center mb-3">
-                            <hr class="h-0 border-b border-solid border-grey-500 grow">
-                            <p class="mx-4 text-grey-600">ou</p>
-                            <hr class="h-0 border-b border-solid border-grey-500 grow">
-                        </div>
-                        <label for="email" class="mb-2 text-sm text-start text-grey-900">Email</label>
-                        <input id="email" type="email" placeholder="exemple@test.com"
-                            class="flex items-center w-full px-5 py-4 mr-2 text-sm font-medium outline-none focus:bg-grey-400 mb-7 shadow-sm border-[1px] border-gray-100 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-md" />
-                        <label for="password" class="mb-2 text-sm text-start text-grey-900">Mot de passe</label>
-                        <input id="password" type="password" placeholder="Entez votre mot de passe"
-                            class="flex items-center w-full px-5 py-4 mb-5 mr-2 text-sm font-medium outline-none focus:bg-grey-400 shadow-sm border-[1px] border-gray-100 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-md" />
-                        <div class="flex flex-row justify-between mb-8">
-                            <label class="relative inline-flex items-center mr-3 cursor-pointer select-none">
-                                <input type="checkbox" checked value="" class="sr-only peer">
-                                <div
-                                    class="w-5 h-5 bg-white border-2 rounded-sm border-grey-500 peer peer-checked:border-0 peer-checked:bg-purple-blue-500">
-                                    <img class=""
-                                        src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/motion-tailwind/img/icons/check.png"
-                                        alt="tick">
-                                </div>
-                            </label>
-                            <a href="javascript:void(0)" class="mr-4 text-sm font-medium text-purple-blue-500">Mot de
-                                passe oublié?</a>
-                        </div>
-                        <button
-                            class="w-full px-6 py-5 mb-5 text-sm font-bold bg-blue-400 leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500">Se
-                            connecter</button>
-                        <p class="text-sm leading-relaxed text-gray-600">Pas encore inscrit?<a
-                                href="{{ route('register.customer') }}" class="font-bold text-blue-500">S'inscrire</a>
-                        </p>
-                    </form>
-                </div>
-            </div>
-        </div>
-</body>
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
-</html>
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- Pharmacie/routes/web.php
+++ Pharmacie/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\MagasinController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SuperAdminController;
-use App\Http\Controllers\ProduitController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,41 +14,18 @@ use App\Http\Controllers\ProduitController;
 |
 */
 
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-
-//SuperAdmin
-Route::middleware(['auth', 'superadmin'])->group(function () {
-    Route::get('/superadmin/dashboard', [SuperAdminController::class, 'dashboard'])->name('superadmin.dashboard');
-    // Ajoutez d'autres routes pour les fonctionnalités du super administrateur
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Account
-Route::controller(AccountController::class)->group(function () {
-    Route::get('/', 'welcome')->name('welcome');
-    Route::get('login', 'login')->name('login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    //Registration
-    Route::get('register', 'register')->name('register.customer');
-    Route::post('register', 'store')->name('register.customer.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-//Magasin
-Route::get('/magasin', [MagasinController::class, 'index'])->name('magasins.index');
-Route::get('/magasin/create', [MagasinController::class, 'create'])->name('magasins.create');
-Route::post('/magasin', [MagasinController::class, 'store'])->name('magasins.store');
-Route::delete('/magasin/{id}', [MagasinController::class, 'destroy'])->name('magasin.destroy');
-
-//Produits
-Route::get('/produits', [ProduitController::class, 'index'])->name('produits.index');
-Route::get('/produits/create', [ProduitController::class, 'create'])->name('produits.create');
-Route::post('/produits/store', [ProduitController::class, 'store'])->name('produits.store');
-Route::get('/produits/{id}/edit', [ProduitController::class, 'edit'])->name('produits.edit');
-Route::put('/produits/{id}', [ProduitController::class, 'update'])->name('produits.update');
-Route::delete('/produits/{id}', [ProduitController::class, 'destroy'])->name('produits.destroy');
+require __DIR__.'/auth.php';
